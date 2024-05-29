e47a0629b0a67ea79da15ad4f1c72554bf110b34
Omar Berrou <omar_berrou@hotmail.fr>
2024-05-29 14:17:38 +0200

    commit

--- fronted/src/pages/homepage.js
+++ fronted/src/pages/homepage.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import BarList from '../components/BarList.js';
 import OrderForm from '../components/orderForm.js';
+import AddBarForm from '../components/addBarForm.js';
 
-const HomePage = () => (
-  <div>
-    <h1>Home Page</h1>
-    <BarList />
-    <OrderForm />
-  </div>
-);
+const HomePage = () => {
+  const [bars, setBars] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/bars');
+        setBars(response.data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+
+    // Clean-up function
+    return () => {
+      setBars([]);
+      setError(null);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {error && <p>Une erreur s'est produite : {error.message}</p>}
+      <h2>Liste des bars :</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Nom</th>
+            <th>Adresse</th>
+            <th>Téléphone</th>
+            <th>Email</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bars.map((bar) => (
+            <tr key={bar.id}>
+              <td>{bar.name}</td>
+              <td>{bar.adresse}</td>
+              <td>{bar.tel}</td>
+              <td>{bar.email}</td>
+              <td>{bar.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+    </div>
+  );
+};
 
 export default HomePage;
